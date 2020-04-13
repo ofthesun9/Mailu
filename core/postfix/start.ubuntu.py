5adb151fc89c388c77aac11471566f6745988820
ofthesun9 <olivier@ofthesun.net>
2020-04-13 14:31:00 +0000

    Moving to ubuntu as base image for postfix and DANE is working

---
+++ core/postfix/start.ubuntu.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+
+import os
+import glob
+import shutil
+import multiprocessing
+import logging as log
+import sys
+
+from podop   import run_server
+from socrate import system, conf
+
+log.basicConfig(stream=sys.stderr, level=os.environ.get("LOG_LEVEL", "WARNING"))
+
+def start_podop():
+    os.setuid(102)
+    url = "http://" + os.environ["ADMIN_ADDRESS"] + "/internal/postfix/"
+    # TODO: Remove verbosity setting from Podop?
+    run_server(0, "postfix", "/tmp/podop.socket", [
+		("transport", "url", url + "transport/§"),
+		("alias", "url", url + "alias/§"),
+		("domain", "url", url + "domain/§"),
+        ("mailbox", "url", url + "mailbox/§"),
+        ("recipientmap", "url", url + "recipient/map/§"),
+        ("sendermap", "url", url + "sender/map/§"),
+        ("senderaccess", "url", url + "sender/access/§"),
+        ("senderlogin", "url", url + "sender/login/§")
+    ])
+
+def is_valid_postconf_line(line):
+    return not line.startswith("#") \
+            and not line == ''
+
+# Actual startup script
+os.environ["FRONT_ADDRESS"] = system.get_host_address_from_environment("FRONT", "front")
+os.environ["ADMIN_ADDRESS"] = system.get_host_address_from_environment("ADMIN", "admin")
+os.environ["ANTISPAM_MILTER_ADDRESS"] = system.get_host_address_from_environment("ANTISPAM_MILTER", "antispam:11332")
+os.environ["LMTP_ADDRESS"] = system.get_host_address_from_environment("LMTP", "imap:2525")
+
+for postfix_file in glob.glob("/conf/*.cf"):
+    conf.jinja(postfix_file, os.environ, os.path.join("/etc/postfix", os.path.basename(postfix_file)))
+
+if os.path.exists("/overrides/postfix.cf"):
+    for line in open("/overrides/postfix.cf").read().strip().split("\n"):
+        if is_valid_postconf_line(line):
+            os.system('postconf -e "{}"'.format(line))
+
+if os.path.exists("/overrides/postfix.master"):
+    for line in open("/overrides/postfix.master").read().strip().split("\n"):
+        if is_valid_postconf_line(line):
+            os.system('postconf -Me "{}"'.format(line))
+
+for map_file in glob.glob("/overrides/*.map"):
+    destination = os.path.join("/etc/postfix", os.path.basename(map_file))
+    shutil.copyfile(map_file, destination)
+    os.system("postmap {}".format(destination))
+    os.remove(destination)
+
+if "RELAYUSER" in os.environ:
+    path = "/etc/postfix/sasl_passwd"
+    conf.jinja("/conf/sasl_passwd", os.environ, path)
+    os.system("postmap {}".format(path))
+
+# Run Podop and Postfix
+multiprocessing.Process(target=start_podop).start()
+os.system("/etc/postfix/post-install meta_directory=/etc/postfix create-missing")
+os.system("postfix start-fg")
